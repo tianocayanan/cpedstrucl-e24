816eca77635fdb8fcb0889487fdb96d0601e2e54
Christian Philip Cayanan <cayananchristian45@gmail.com>
2016-12-17 10:07:52 +0800

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -1,73 +1,139 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 using namespace std;
 
-void strcat(char *n1, char *n2)
-void strcpy(char *n1, char *n2)
-void strrev(char *n1, char *n2)
-void strcmp(char *n1, char *n2)
+void copy(char *mess3,char *mess1);
 
-int main(){
-	int choice = 0;
-	char firstMessage, secondMessage;
-	cout<<"Enter first message: ";
-	gets(firstMessage);
-	cout<<"Enter second message: ";
-	gets(secondMessage);
-	getch();
-	while(1){
-		cout<<"MAIN MENU\n";
-		cout<<"[1] - strcat\n";
-		cout<<"[2] - strcpy\n";
-		cout<<"[3] - strrev\n";
-		cout<<"[4] - strcmp\n";
-		cout<<"[5] - Exit\n";
-		cout<<endl;
-		cout<<"Enter your choice: ";
-		cin>>choice;
-		switch(choice){
-			case 1: strcat(firstMessage, secondMessage);
+void concat(char *mess1, char *mess2);
+
+void reverse(char *mess1);
+
+int comp(char *mess1, char *mess2);
+
+void menu();
+
+int string_length(char*);
+
+int main()
+{
+	int choice, mess;
+	char mess1[20], mess2[20], mess3[20];
+	main:
+	menu();
+	cin >>choice;
+	system("cls");
+	switch (choice){
+
+		case 1:
+			cin.ignore();
+			cout<<"Enter message: ";
+			gets(mess1);
+			copy(mess3, mess1);
+			cout << "Copied message: " <<mess3;
 			break;
-			case 2: strcpy(firstMessage, secondMessage);
+		case 2:
+			cin.ignore();
+			cout << "Enter first message: ";
+			gets(mess1);
+			cout << "Enter second message: ";
+			gets(mess2);
+			concat(mess1,mess2);
+			cout<<"\nConcatinate message: " <<mess1;
 			break;
-			case 3: strrev(firstMessage, secondMessage);
+		case 3:
+			cin.ignore();
+			cout <<"Enter message: ";
+			gets(mess1);
+			reverse(mess1);
+			cout <<"\nReverse message: " <<mess1;
 			break;
-			case 4: strcmp(firstMessage, secondMessage);
+		case 4:
+			cin.ignore();
+			cout <<"Enter first message: ";
+			gets(mess1);
+			cout << "Enter second message: ";
+			gets(mess2);
+			comp(mess1,mess2);
+			if (comp(mess1,mess2))
+			cout <<"Both message are equal" <<endl;
+			else
+			cout <<"Both messages are not equal" <<endl;
 			break;
-			case 5: cout<<"\nThank you for using the program!\n";
+		case 5:
 			exit(1);
-			break;
-			default: cout<<"INVALID CHOICE! Please try again.";
-		}
+		default:
+			cout <<"\nInvalid Input";
+			goto main;
+			
 	}
-	cout<<endl;
-	system("pause");
 }
 
-void strcat(char *n1, char *n2){
-	while(*n1!='\0'){
-		cout<<*n1;
-	}
-	while(*n2!='\0'){
-		cout<<*n2;	
-	}
-	getch();
-}
-
-void strcpy(char *n1, char *n2){
-	while(*n1!='\0'||*n2!='0'){
-		*n1=*n2;
-		cout<<*n1;
-		*n1++;
-		*n2++;
-	}
-	getch();
-}
-
-void strrev(){
-	int *n1=0
-}
-
-void strcmp(){
+void menu()
+{
+	system("cls");
+	cout <<endl <<"Main Mennu\n";
+	cout <<endl;
+	cout <<"[ 1 ] strcpy . . .\n";
+	cout <<"[ 2 ] strcat . . .\n";
+	cout <<"[ 3 ] strrev . . .\n";
+	cout <<"[ 4 ] strcmp . . .\n";
+	cout <<"[ 5 ] Exit . . .\n";
 	
+	cout <<"\n Enter: ";
+
+}
+
+void copy(char *mess1, char *mess3)
+{
+	while(*mess1++ = *mess3++);
+}
+
+void concat(char *mess1, char *mess2)
+{
+	while(*mess1)
+		*mess1++;
+	while(*mess1++ = *mess2++);
+}
+
+void reverse(char *mess1)
+
+{
+	int length, c;
+	char *begin, *end, temp;
+	length = string_length(mess1);
+	begin = mess1;
+	end = mess1;
+	for(c=0;c<(length-1);c++)
+		end++;
+	for(c=0;c<length/2;c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+		begin++;
+		end--;
+	}
+	
+	
+	
+}
+
+int string_length(char *pointer)
+{
+	int c = 0;
+	while(*(pointer+c)!='\0')
+		c++;
+	return c;
+}
+
+int comp(char *s1, char *s2){
+	while(*s1 == *s2){
+		if(*s1 == '\0' || *s2 == '\0')
+			break;
+		s1++;
+		s2++;
+	}
+	if(*s1 == '\0' && *s2 == '\0')
+		return 1;
+	return 0;
 }
