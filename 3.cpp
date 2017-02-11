656057786bfab173ed49df1c3443e561e76409c5
Christian Philip Cayanan <cayananchristian45@gmail.com>
2017-02-11 11:59:45 +0800

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 struct Tree {
 	char data;
-	Tree *left;
-	Tree *right;  
-	Tree *parent;  
+	Tree *left *right, *parent; 
 };
 
 struct Tree *newTreeNode(int data) {
@@ -14,22 +12,18 @@ struct Tree *newTreeNode(int data) {
 	node->left = NULL;
 	node->right = NULL;
 	node->parent = NULL;
-
 	return node;
 }
 
 struct Tree* insertTreeNode(struct Tree *node, int data) {
 	static Tree *p;
 	Tree *retNode;
-
-	//if(node != NULL) p = node;
-
 	if(node == NULL)  {
 	    retNode = newTreeNode(data);
 	    retNode->parent = p;
 	    return retNode;
 	}
-	if(data <= node->data ) { 
+	if(data <= node->data) { 
 	    p = node;
 	    node->left = insertTreeNode(node->left,data);
 	} 
@@ -40,42 +34,21 @@ struct Tree* insertTreeNode(struct Tree *node, int data) {
 	return node;
 }
 
-/* print tree in order */
-/* 1. Traverse the left subtree. 
-   2. Visit the root. 
-   3. Traverse the right subtree. 
-*/
-
 void printTreeInOrder(struct Tree *node) {
 	if(node == NULL) return;
-
 	printTreeInOrder(node->left);
 	cout << node->data << " ";
 	printTreeInOrder(node->right);
 }
 
-/* print tree in postorder*/
-/* 1. Traverse the left subtree. 
-   2. Traverse the right subtree. 
-   3. Visit the root. 
-*/
 void printTreePostOrder(struct Tree *node) {
 	if(node == NULL) return;
-
 	printTreePostOrder(node->left);
 	printTreePostOrder(node->right);
 	cout << node->data << " ";
 }
 
-int main(int argc, char **argv) {
-	char ch, ch1, ch2;
-	Tree *found;
-	Tree *succ;
-	Tree *pred;
-	Tree *ancestor;
-	char charArr[9] 
-	    = {'A','B','C','D','E','F','G','H','I'};
-
+int main() {
 	Tree *root = newTreeNode('F');
 	insertTreeNode(root,'B');
 	insertTreeNode(root,'A');
@@ -85,16 +58,13 @@ int main(int argc, char **argv) {
 	insertTreeNode(root,'G');
 	insertTreeNode(root,'I');
 	insertTreeNode(root,'H');
-
-	/* print tree in order */
+	
 	cout << "increasing sort order\n";
 	printTreeInOrder(root);
 	cout << endl;
-
-	/* print tree in postorder*/
+	
 	cout << "post order \n";
 	printTreePostOrder(root);
 	cout << endl;
-
 	return 0;
 }
